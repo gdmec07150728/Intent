a218dee52f73ae5de6d50b16d3bcc904b2e0bb5d
junfeng <863462463@qq.com>
2016-10-29 15:00:41 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150728/intent/IntentDemo3.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.s07150728.intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administartor on 2016/10/29.
+ */
+public class IntentDemo3 extends AppCompatActivity{
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
